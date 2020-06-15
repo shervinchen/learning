328960e42d581be1579d02af33e8278e00c3638a
csdoker <csd758371536@qq.com>
2020-06-15 15:46:12 +0800

    update react-epic

--- jirengu/react-epic/src/components/Uploader.js
+++ jirengu/react-epic/src/components/Uploader.js
@@ -1,23 +1,18 @@
 import React, { useRef } from "react";
 import { useStores } from "../stores";
 import { observer } from "mobx-react";
-import { Upload } from 'antd'
-const { Dragger } = Upload
+import { Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
 
-const props = {
-    beforeUpload: file => {
-        return false
-    }
-}
+const { Dragger } = Upload;
 
 const Component = observer(() => {
   const { ImageStore } = useStores();
-  const ref = useRef();
-  const bindChange = () => {
-    console.log(ref.current);
-    if (ref.current.files.length > 0) {
-      ImageStore.setFile(ref.current.files[0]);
-      ImageStore.setFilename(ref.current.files[0].name);
+  const props = {
+    showUploadList: false,
+    beforeUpload: (file) => {
+      ImageStore.setFile(file);
+      ImageStore.setFilename(file.name);
       ImageStore.upload()
         .then((serverFile) => {
           console.log("上传成功");
@@ -26,14 +21,30 @@ const Component = observer(() => {
         .catch(() => {
           console.log("上传失败");
         });
-    }
-    window.file = ref.current;
+      return false;
+    },
   };
 
   return (
     <div>
-      <h1>文件上传</h1>
-      <input type="file" ref={ref} onChange={bindChange} />
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibit from uploading
+          company data or other band files
+        </p>
+      </Dragger>
+      <div>
+        <h1>上传结果</h1>
+        {ImageStore.serverFile ? (
+          <div>{ImageStore.serverFile.attributes.url.attributes.url}</div>
+        ) : null}
+      </div>
     </div>
   );
 });
