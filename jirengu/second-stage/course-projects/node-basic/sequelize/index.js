124a65bf8be92c5435ea8eb0b4217c64a26323b9
shervinchen <shervinchen@gmail.com>
2024-09-30 13:48:54 +0300

    chore: update dependencies

--- jirengu/second-stage/course-projects/node-basic/sequelize/index.js
+++ jirengu/second-stage/course-projects/node-basic/sequelize/index.js
@@ -1,6 +1,9 @@
+// node index.js
+
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('nodemysql', 'root', '123456', {
   // host: '',
+  port: '3307',
   dialect: 'mysql',
 });
 
@@ -13,26 +16,26 @@ User.init(
   { sequelize, modelName: 'user' }
 );
 
-async function run() {
-  User.destroy({
-    where: {
-      id: 1,
-    },
-  });
-  const users = await User.findAll();
-  console.log(JSON.stringify(users));
-  sequelize.close();
-}
-run();
-
-// sequelize
-//   .sync()
-//   .then(() =>
-//     User.create({
-//       username: 'janedoe',
-//       birthday: new Date(1980, 6, 20),
-//     })
-//   )
-//   .then((jane) => {
-//     console.log(jane.toJSON());
+// async function run() {
+//   User.destroy({
+//     where: {
+//       id: 1,
+//     },
 //   });
+//   const users = await User.findAll();
+//   console.log(JSON.stringify(users));
+//   sequelize.close();
+// }
+// run();
+
+sequelize
+  .sync()
+  .then(() =>
+    User.create({
+      username: 'janedoe',
+      birthday: new Date(1980, 6, 20),
+    })
+  )
+  .then((jane) => {
+    console.log(jane.toJSON());
+  });
